b10ec0850d77bbd65d730544db5e44d05f54f40c
pvtryk <patryk_ch@yahoo.com>
2022-04-11 20:44:10 +0200

    feat(data): added example data for seed

---
+++ data/meals.ts
@@ -0,0 +1,29 @@
+export const meals = [
+  {
+    id: '1',
+    name: 'Pad-thai',
+    description: 'is a stir-fried rice noodle dish commonly served as a street food in Thailand as part of the country\'s cuisine.',
+    ingredients: ['rice noodles', 'eggs', 'chicken'],
+    kcal: 100,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Phat_Thai_kung_Chang_Khien_street_stall.jpg/440px-Phat_Thai_kung_Chang_Khien_street_stall.jpg',
+    category: 'Street food',
+  },
+  {
+    id: '2',
+    name: 'Cheesburger',
+    description: 'is a hamburger topped with cheese.',
+    ingredients: ['beef', 'cheese', 'bun'],
+    kcal: 170,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Cheeseburger_with_fries.jpg/600px-Cheeseburger_with_fries.jpg',
+    category: 'Street food',
+  },
+  {
+    id: '3',
+    name: 'Pizza',
+    description: 'is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients',
+    ingredients: ['tomato sauce', 'cheese'],
+    kcal: 120,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/440px-Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+    category: 'Dinner',
+  }
+]
